49194bf37f550589f1165132f979fc510bf29b23
Rob Convery <robc@madebysauce.com>
2021-03-13 22:44:41 +0000

    Initial commit

---
+++ resources/views/any.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{env('APP_NAME')}}</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+    <!-- TrustBox script -->
+    <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+    <!-- End TrustBox script -->
+</head>
+<body>
+    <app :proposal="{{ $proposalJson }}"></app>
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
